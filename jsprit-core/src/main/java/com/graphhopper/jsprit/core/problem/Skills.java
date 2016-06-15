d6f336dd8af261f163f9bc508432fa9bd12405f4
braktar <rgwenael@gmail.com>
2016-06-15 11:08:32 +0200

    Fix alternative skills

--- jsprit-core/src/main/java/com/graphhopper/jsprit/core/problem/Skills.java
+++ jsprit-core/src/main/java/com/graphhopper/jsprit/core/problem/Skills.java
@@ -20,7 +20,6 @@ package com.graphhopper.jsprit.core.problem;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 /**
